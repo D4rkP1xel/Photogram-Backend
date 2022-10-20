992b3c775595f3ab4666caa0c201d569dda290b2
Alex <alexcruzsilva12@gmail.com>
2022-10-20 19:13:40 +0100

    deploymentv2

---
+++ Routes/posts.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const cloudinary = require('cloudinary')
+
+//INSERT INTO POSTS VALUES('abc','abc','abc','1','abc', UTC_TIMESTAMP);
+
+module.exports = router
