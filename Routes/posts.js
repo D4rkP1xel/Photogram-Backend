65685aa54bbd6daf01b0b06cde9cf466f7e23886
Alex <alexcruzsilva12@gmail.com>
2022-10-25 19:55:32 +0100

    add new post route

--- Routes/posts.js
+++ Routes/posts.js
@@ -1,16 +1,45 @@
 const router = require('express').Router()
-const cloudinary = require('cloudinary')
 const mysql = require('mysql2/promise')
 //INSERT INTO POSTS VALUES('abc','abc','abc','1','abc', UTC_TIMESTAMP);
+const cloudinary = require('cloudinary')
 
-router.get("/user/:id", async(req, res) =>{
-  
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+router.get("/user/:id", async (req, res) => {
+
     const user_id = req.params.id
     console.log(req.params.id)
     const connection = await mysql.createConnection(process.env.DATABASE_URL)
     const query = `SELECT * FROM POSTS WHERE user_id='${user_id}';`
     const [rows] = await connection.query(query)
     console.log(rows)
-    res.status(200).json({message:"success", data: rows})
+    res.status(200).json({ message: "success", data: rows })
+})
+
+router.post("/newPost", async (req, res) => {
+    console.log(req.body)
+    if (req.body.image == null || req.body.description == undefined || req.body.user_id == null || req.body.tags == undefined) {
+        res.status(403).json({ message: "ERROR: wrong params", })
+        return
+    }
+    try {
+        const base64Image = req.body.image
+        const uploadedResponse = await cloudinary.v2.uploader.upload(base64Image, { upload_preset: "preset_user_photo" })
+        const photo_url = uploadedResponse.secure_url
+        const connection = await mysql.createConnection(process.env.DATABASE_URL)
+        const id = Date.now().toString() + Math.floor(Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)).toString(36) //lenght 22
+        const query = `INSERT INTO POSTS VALUES ('${req.body.user_id}', 1, '${photo_url}', UTC_TIMESTAMP, '${id}')`
+        await connection.query(query)
+        res.status(200).json({message: "success"})
+    }
+    catch (err) {
+        console.log(err)
+        res.status(503).json({message: "ERROR: Server error"})
+    }
+
 })
 module.exports = router
